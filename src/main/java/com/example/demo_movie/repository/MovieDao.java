e9f728c19d066a0844b68f2294781e5f16712b6f
User <User@DESKTOP-CGEU4AU>
2022-12-05 16:15:27 +0800

    findMovieByMovieName

--- src/main/java/com/example/demo_movie/repository/MovieDao.java
+++ src/main/java/com/example/demo_movie/repository/MovieDao.java
@@ -1,5 +1,7 @@
 package com.example.demo_movie.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import com.example.demo_movie.entity.Movie;
 
 @Repository
 public interface MovieDao extends JpaRepository<Movie, String>{
+
+	public List<Movie> findMovieByMovieName(String movieName);
+	
+	public List<Movie> findMovieByType(String type);
 
 }
